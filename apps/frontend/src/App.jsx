d50b9c497f7a2270b75cc762da3c4c726c66a409
Sami M. <sami.mal@outlook.com>
2025-02-03 23:05:31 +0300

    frontend: Add theme support

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 // Layouts
 import RootLayout from "./views/layouts/RootLayout";
@@ -14,22 +15,24 @@ import {
 
 function App() {
   return (
-    <Routes>
-      {/* public routes */}
-      <Route path="register" element={<RegisterPage />} />
-      <Route path="signin" element={<LoginPage />} />
+    <ThemeProvider defaultTheme="system" storageKey="happymap-theme">
+      <Routes>
+        {/* public routes */}
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="signin" element={<LoginPage />} />
 
-      {/* TODO: add a 404 page and 500 page with redirect button to home */}
-      <Route path="/oops" element={<div>Oops</div>} />
-      <Route path="/404" element={<div>404</div>} />
+        {/* TODO: add a 404 page and 500 page with redirect button to home */}
+        <Route path="/oops" element={<div>Oops</div>} />
+        <Route path="/404" element={<div>404</div>} />
 
-      {/* private routes */}
-      <Route element={<RootLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="explore" element={<ExplorePage />} />
-        <Route path="profile" element={<ProfilePage />} />
-      </Route>
-    </Routes>
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="explore" element={<ExplorePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
