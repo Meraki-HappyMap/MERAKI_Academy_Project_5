0c961cbc89825dd3772f5025ce26ee0f7a9049c7
AhmadAWafa <ahmad.abdoullah.wafa@gmail.com~>
2025-02-08 20:31:48 +0300

    frontend:404page,oopspage

--- apps/frontend/src/views/pages/index.js
+++ apps/frontend/src/views/pages/index.js
@@ -1,6 +1,9 @@
 // Pages
+
 export { default as HomePage } from "./HomePage";
 export { default as ExplorePage } from "./ExplorePage";
 export { default as ProfilePage } from "./ProfilePage";
-export { default as SigninPage } from "./SigninPage.jsx";
-export { default as RoleSelectionPage } from "./RoleSelectionPage";
+export { default as LoginPage } from "./LoginPage";
+export { default as RegisterPage } from "./RegisterPage";
+export { default as NotFoundPage } from "./404Page";
+export { default as OopsPage } from "./OopsPage";
