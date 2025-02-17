dc46d0d2b142357081e78ebb99f886ef9f06a374
SalehAlRawajfeh <alrawajfa.s@gmail.com>
2025-02-17 20:56:07 +0300

    frontend: booking user

--- apps/frontend/src/components/manage-Booking/PlaceInfo.jsx
+++ apps/frontend/src/components/manage-Booking/PlaceInfo.jsx
@@ -3,31 +3,61 @@ import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 const placeData = [
-  {
-    id: 2,
-    name: "game center",
-    category_id: 1,
-  },
-  {
-    id: 3,
-    name: "game center",
-    category_id: 1,
-  },
-  {
-    id: 4,
-    name: "test3",
-    category_id: 2,
-  },
-  {
-    id: 5,
-    name: "test",
-    category_id: 2,
-  },
-  {
-    id: 7,
-    name: "test",
-    category_id: 2,
-  },
+    {
+        id: 2,
+        name: "game center",
+        description: "anythig",
+        user_id: 1,
+        category_id: 1,
+        location: "test",
+        is_deleted: 0,
+        created_at: "2025-02-04T18:22:37.112Z",
+        updated_at: "2025-02-04T18:22:37.112Z"
+      },
+      {
+        id: 3,
+        name: "game center",
+        description: "updated",
+        user_id: 1,
+        category_id: 1,
+        location: "test",
+        is_deleted: 0,
+        created_at: "2025-02-04T18:23:55.369Z",
+        updated_at: "2025-02-04T18:24:12.469Z"
+      },
+      {
+        id: 4,
+        name: "test3",
+        description: "test also",
+        user_id: 2,
+        category_id: 2,
+        location: "test",
+        is_deleted: 0,
+        created_at: "2025-02-10T07:42:55.375Z",
+        updated_at: "2025-02-10T07:42:55.375Z"
+      },
+      {
+        id: 5,
+        name: "test",
+        description: "test",
+        user_id: 2,
+        category_id: 2,
+        location: "test",
+        is_deleted: 0,
+        created_at: "2025-02-12T14:45:20.462Z",
+        updated_at: "2025-02-12T14:45:20.462Z"
+      },
+      {
+        id: 7,
+        name: "test",
+        description: "test",
+        user_id: 1,
+        category_id: 2,
+        location: "test",
+        is_deleted: 0,
+        created_at: "2025-02-15T11:04:25.135Z",
+        updated_at: "2025-02-15T11:04:25.135Z"
+      }
 ];
 
 function PlaceInfo({ booking }) {
